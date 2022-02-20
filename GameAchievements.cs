c06b1f0855b3060016c83c3dcb7c32a2bd3fbd10
Tai Woodley <taiwoodley@gmail.com>
2022-02-21 09:51:24 +1100

    Implement achievement advice from unowned achievements

--- GameAchievements.cs
+++ GameAchievements.cs
@@ -18,6 +18,7 @@ namespace steamachievements
         public int TotalAchievements { get; set; }
         public int OwnedAchievements { get; set; }
         public decimal gamePercentage { get; set; }
+        public int sortScore { get; set; }
 
         public static async Task analyseAchievements(ISteamWebResponse<PlayerSummaryModel> playerSummaryResponse, OwnedGamesResultModel games, SteamUserStats steamUserStats, ulong userID)
         {
@@ -69,27 +70,7 @@ namespace steamachievements
                     gameAchievements.Add(gameStats);
                 }
             }
-            getAchievementInput(gameAchievements);
-        }
-
-        private static void getAchievementInput(List<GameAchievements> gameAchievements)
-        {
-            while (true)
-            {
-                string input = Program.prompt("What would you like about your achievements?");
-                switch (input)
-                {
-                    case "stats":
-                        CalculateAccountStats(gameAchievements);
-                        return;
-                    case "advice":
-                        Console.WriteLine("Vex y and z");
-                        return;
-                    default:
-                        Console.WriteLine("Option not found. Valid options are \"stats\" and \"advice\".");
-                        continue;
-                }
-            }
+            CalculateAccountStats(gameAchievements);
         }
 
         public static void CalculateAccountStats(List<GameAchievements> gameAchievements)
@@ -107,8 +88,46 @@ namespace steamachievements
             var eligibleGames = gameAchievements.Count(game => game.OwnedAchievements != 0 && game.TotalAchievements != 0);
             var averagePercentage = totalPercentage / eligibleGames;
             var roundedPercentage = Math.Round(averagePercentage, 2);
-            Console.WriteLine($"Vexo has {earnedAchievementTotal} achievements out of a possible {achievementTotal} achievements");
+            Console.WriteLine($"This user has {earnedAchievementTotal} achievements out of a possible {achievementTotal} achievements");
             Console.WriteLine($"This comes to an average completion percentage of {roundedPercentage}");
+
+            SelectAlgorithm(gameAchievements);
+        }
+
+        private static void SelectAlgorithm(List<GameAchievements> gameAchievements)
+        {
+            while (true)
+            {
+                var input = Program.prompt("How would you like your achievements analysed?");
+                switch (input.ToLower())
+                {
+                    case "unowned":
+                        findClosetoCompletion(gameAchievements);
+                        return;
+                    default:
+                        Console.WriteLine("Invalid selection. Please type \"help\" for a list of valid selections.");
+                        continue;
+                }
+            }
+        }
+
+        private static void findClosetoCompletion(List<GameAchievements> gameAchievements)
+        {
+            // Recommend games based on the number of unowned achievements.
+            foreach (var game in gameAchievements)
+            {
+                game.sortScore = game.TotalAchievements - game.OwnedAchievements;
+            }
+            gameAchievements.RemoveAll(game => game.sortScore == 0);
+            gameAchievements = gameAchievements.OrderBy(game => game.sortScore).ToList();
+            var showAchievementsCount = 5;
+            var count = 0;
+            Console.WriteLine("Based on the number of unowned achievements, the following games are close to completion:");
+            while (count != showAchievementsCount)
+            {
+                Console.WriteLine($"{gameAchievements[count].Name} has {gameAchievements[count].sortScore} unowned achievements.");
+                count++;
+            }
         }
     }
 }
